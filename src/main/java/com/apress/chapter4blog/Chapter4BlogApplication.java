4fd602a8b21423949d015d1feeb5e45904a65b55
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2026-03-08 15:26:59 +0300

    first commit

---
+++ src/main/java/com/apress/chapter4blog/Chapter4BlogApplication.java
@@ -0,0 +1,13 @@
+package com.apress.chapter4blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter4BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter4BlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apress/chapter4blog/Chapter4BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apress.chapter4blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter4BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
